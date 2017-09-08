5f932a74d7e9ee5950e397e235ac5a44fa0532a2
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-09-09 00:19:49 +1000

    Rubicon is now supported

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,3 +1,4 @@
 export { Snowplow } from './snowplow.js';
 export { AdobeAudienceManager } from './adobe_audience_manager.js';
 export { Krux } from './krux.js';
+export { Rubicon } from './rubicon.js';
