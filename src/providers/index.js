81f63c58e5c6ddef55391259de79e72370d861cf
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-09-09 00:37:33 +1000

    Nielsen is now supported

--- src/providers/index.js
+++ src/providers/index.js
@@ -2,3 +2,4 @@ export { Snowplow } from './snowplow.js';
 export { AdobeAudienceManager } from './adobe_audience_manager.js';
 export { Krux } from './krux.js';
 export { Rubicon } from './rubicon.js';
+export { Nielsen } from './nielsen.js';
