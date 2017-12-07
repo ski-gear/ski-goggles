826c287bf4ff637d055534aefe2d8cdb9e2f74bf
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-07 22:20:59 +1100

    Update version

--- src/versions.js
+++ src/versions.js
@@ -2,5 +2,5 @@
 
 import type { Version } from './types';
 
-export const AppVersion: Version = '1.2.1';
-export const UserOptionsVersion: Version = '1.2.1';
+export const AppVersion: Version = '1.3.0';
+export const UserOptionsVersion: Version = '1.3.0';
