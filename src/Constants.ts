508558d9ebdf65ee25ce4092db15e41ccbc98d00
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-23 23:54:03 +1100

    Enable default providers

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,6 +1,8 @@
 import { RunTimeMessage } from "./types/Types";
+import { ProviderCanonicalName } from "ski-providers";
 
 export const GIT_ISSUES_URL: string = "https://github.com/ski-gear/ski-goggles/issues";
 export const OPEN_OPTIONS_TAB: RunTimeMessage = "open-options-tab";
 export const OPEN_ISSUES_PAGE: RunTimeMessage = "open-issues-page";
 export const MaxRequestsDisplayed = 30;
+export const PreferredProviders: ProviderCanonicalName[] = ["Snowplow"];
