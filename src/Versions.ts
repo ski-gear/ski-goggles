0c13b6ad329c548a7245973c9e0963b19dfcaedf
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-23 23:59:05 +1100

    v1.4.0

--- src/Versions.ts
+++ src/Versions.ts
@@ -1,4 +1,4 @@
 import { Version } from './types/Types';
 
-export const AppVersion: Version = '1.3.0';
-export const UserOptionsVersion: Version = '1.3.0';
+export const AppVersion: Version = '1.4.0';
+export const UserOptionsVersion: Version = '1.4.0';
