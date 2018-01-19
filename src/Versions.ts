abe1c5d5db677fc0de9c130e48326d6b0a786789
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2018-01-19 22:44:13 +1100

    Bump release version

--- src/Versions.ts
+++ src/Versions.ts
@@ -1,4 +1,4 @@
 import { Version } from "./types/Types";
 
-export const AppVersion: Version = "1.5.1";
-export const UserOptionsVersion: Version = "1.5.1";
+export const AppVersion: Version = '1.5.2';
+export const UserOptionsVersion: Version = '1.5.2';
