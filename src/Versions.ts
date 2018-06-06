773ea92e78cb17b604ecb0ada5f41cd787783aa2
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2018-06-06 22:37:13 +1000

    Bump up the version to 1.6.0

--- src/Versions.ts
+++ src/Versions.ts
@@ -1,4 +1,4 @@
 import { Version } from "./types/Types";
 
-export const AppVersion: Version = '1.5.4';
-export const UserOptionsVersion: Version = '1.5.4';
+export const AppVersion: Version = '1.6.0';
+export const UserOptionsVersion: Version = '1.6.0';
