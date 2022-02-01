9919489f3b47847b4f02d8715378c94e948379d7
Shawn Foo <ioguS@users.noreply.github.com>
2022-02-01 14:07:39 +1100

    Manifest v3 update
    Adjusted manifest config file format
    Removed deprecated querystring dependency

--- src/Parser.ts
+++ src/Parser.ts
@@ -5,5 +5,6 @@ import { parse as UrlParse } from "url";
 export const parse = (url: string): object => {
   const parsed = UrlParse(url);
   const parsedQuery = defaultTo("", parsed.query) as string;
+  // Need to get rid of the querystring module usage. Refer to URLSearchParams API for new usage.
   return querystring.parse(parsedQuery);
 };
